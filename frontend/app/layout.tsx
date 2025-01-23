605ebbadb590fbb2d3aad5ea391cb3a61fa054b7
Samuel Ramirez Laptop <serr73094@gmail.com>
2025-01-23 11:16:06 -0500

    actualizacion color y logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@livekit/components-styles";
 import "./globals.css";
 import { Public_Sans } from "next/font/google";
+import Image from 'next/image';
 
 const publicSans400 = Public_Sans({
   weight: "400",
@@ -14,7 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`h-full ${publicSans400.className}`}>
-      <body className="h-full">{children}</body>
+      <body className="h-full bg-white relative">
+        <div className="absolute top-4 right-4 z-10">
+          <Image 
+            src="https://www.unisabana.edu.co/fileadmin/Archivos_de_usuario/Imagenes/Imagenes_Gov_Lab/cabezote-govlab-dos-perfiles-azul.png" 
+            alt="Logo" 
+            width={100} 
+            height={50} 
+            priority 
+          />
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
